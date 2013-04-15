e81131831ecff305c1703d803c73f639c7b29f81
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2013-04-15 09:20:00 +0330

    be soorate multiSelect mishe Request ha ro taghir vaziat dad

--- SpareParts/Lib/RequestsObservableCollection.cs
+++ SpareParts/Lib/RequestsObservableCollection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -106,5 +107,6 @@ namespace SpareParts.Lib
 
             return false;
         }
+
     }
 }
